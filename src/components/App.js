bc675f0aa4a4c7a8a54bdafc889a188b67d69e68
travinne <travinne.mutahi@moringaschool.com>
2025-04-08 23:23:22 +0300

    React Components Basics Lab

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,13 @@ function Home() {
     </div>
   );
 }
-
+function About() {
+  return (
+    <div id="about">
+      <h1>Home</h1>
+    </div>
+  );
+}
 {/* write an <About> component here */}
 
 
@@ -23,8 +29,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/> 
     </div>
   );
 }
